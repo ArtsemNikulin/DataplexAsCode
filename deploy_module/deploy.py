6f66f4d07ca634dccadb440cefa9965e8f1fd64f
Artsem_Nikulin <07a.nikulin@gmail.com>
2024-10-02 12:49:28 +0200

    test

--- deploy_module/deploy.py
+++ deploy_module/deploy.py
@@ -50,7 +50,7 @@ class DataScanManager:
             parent=parent,
             data_scan=data_scan,
             data_scan_id=data_scan_id,
-            validate_only=False
+            validate_only=True
         )
 
         try:
