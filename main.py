fb6cba66343fb467e577745d6bb0d29d6376be77
Artsem_Nikulin <07a.nikulin@gmail.com>
2024-10-02 15:09:28 +0200

    test

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from deploy_module.deploy import DataScanManager
-import os
-
-branch_name = os.getenv('BRANCH_NAME').lower()
-print("***********************")
-print(f"Deploy for {branch_name.upper()} environment")
-print("***********************")
-manager = DataScanManager(env=branch_name)
-manager.create_data_scan(validate=True)
+print('Hello')
