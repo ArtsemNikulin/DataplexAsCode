8f4a89f5202ef857e4fde10ae57c8965feb0558e
Artsem_Nikulin <07a.nikulin@gmail.com>
2024-10-07 15:20:09 +0200

    test

--- deploy_module/rules_reader.py
+++ deploy_module/rules_reader.py
@@ -1,14 +1,12 @@
 import yaml
 
 
-
 class RulesReader:
     def __init__(self, pathes):
         self.base_path = pathes
         self.all_rules = []
 
-    def get_rules(self, dataset, table):
-        rules_file_path = self.base_path / dataset / table / 'rules.yaml'
+    def get_rules(self, rules_file_path):
         try:
             with open(rules_file_path, 'r') as file:
                 rules = yaml.safe_load(file)
@@ -18,20 +16,18 @@ class RulesReader:
             raise FileNotFoundError(f"Rules file not found: {rules_file_path}")
 
     def get_datasets_with_rules(self):
-        base_path = self.base_path
-        for dataset_path in base_path.iterdir():
-            if dataset_path.is_dir():
-                dataset_name = dataset_path.name
-                for table_path in dataset_path.iterdir():
-                    if table_path.is_dir() and (table_path / 'rules.yaml').exists():
-                        rules = self.get_rules(dataset_name, table_path.name)
-                        self.all_rules.append({'dataset': dataset_name,
-                                               'table': table_path.name,
-                                               'samplingPercent': rules.get('samplingPercent', 100),
-                                               'rowFilter': rules.get('rowFilter', ""),
-                                               'labels': rules.get('labels', {}),
-                                               'executionSpec': rules.get('executionSpec', {}),
-                                               'rules': rules.get('rules', []),
-                                               })
+        for rules_path in self.base_path:
+            if rules_path.exists() and rules_path.parts[-5] == 'rules' and rules_path.parts[-4] == 'datasets':
+                dataset_name = rules_path.parts[-3]
+                table_name = rules_path.parts[-2]
+                rules = self.get_rules(rules_path)
+                self.all_rules.append({'dataset': dataset_name,
+                                       'table': table_name,
+                                       'samplingPercent': rules.get('samplingPercent', 100),
+                                       'rowFilter': rules.get('rowFilter', ""),
+                                       'labels': rules.get('labels', {}),
+                                       'executionSpec': rules.get('executionSpec', {}),
+                                       'rules': rules.get('rules', []),
+                                       })
 
         return self.all_rules
--- main.py
+++ main.py
@@ -6,16 +6,5 @@ import os
 from deploy_module.git_changes import get_changed_files
 
 
-branch_name = os.getenv('BRANCH_NAME')
-base_branch = os.getenv('BASE_BRANCH')
-pr = os.getenv('PR')
-print("*********************************************")
-print(f"Validation of PR #{pr}: {branch_name} TO {base_branch}")
-print("*********************************************")
-# manager = DataScanManager('dev')
-# manager.form_data_scans()
-x = get_changed_files()
-z = [changed_rules for changed_rules in x if changed_rules.name == 'rules.yaml']
-# z = RulesReader(x).get_datasets_with_rules()
 
-print(z)
+
