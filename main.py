e7f9c03a300c039a3c98eb473d8f2947bffff83e
Artsem_Nikulin <07a.nikulin@gmail.com>
2024-10-07 14:18:59 +0200

    test

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import json
 from deploy_module.deploy import DataScanManager
 from deploy_module.rules_reader import RulesReader
 import os
+from deploy_module.git_changes import get_changed_files
 
 
-
-branch_name = os.getenv('BRANCH_NAME')
-base_branch = os.getenv('BASE_BRANCH')
-pr = os.getenv('PR')
-print("*********************************************")
-print(f"Validation of PR #{pr}: {branch_name} TO {base_branch}")
-print("*********************************************")
-manager = DataScanManager('dev')
-manager.form_data_scans()
+# branch_name = os.getenv('BRANCH_NAME')
+# base_branch = os.getenv('BASE_BRANCH')
+# pr = os.getenv('PR')
+# print("*********************************************")
+# print(f"Validation of PR #{pr}: {branch_name} TO {base_branch}")
+# print("*********************************************")
+# manager = DataScanManager('dev')
+# manager.form_data_scans()
