e62d193fa171c1bdc9f0ba8efa1c5fb0eb4d0774
Artsem_Nikulin <07a.nikulin@gmail.com>
2024-10-07 14:22:19 +0200

    test

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import os
 from deploy_module.git_changes import get_changed_files
 
 
-# branch_name = os.getenv('BRANCH_NAME')
-# base_branch = os.getenv('BASE_BRANCH')
-# pr = os.getenv('PR')
-# print("*********************************************")
-# print(f"Validation of PR #{pr}: {branch_name} TO {base_branch}")
-# print("*********************************************")
-# manager = DataScanManager('dev')
-# manager.form_data_scans()
+branch_name = os.getenv('BRANCH_NAME')
+base_branch = os.getenv('BASE_BRANCH')
+pr = os.getenv('PR')
+print("*********************************************")
+print(f"Validation of PR #{pr}: {branch_name} TO {base_branch}")
+print("*********************************************")
+manager = DataScanManager('dev')
+manager.form_data_scans()
