221c11a5c7b50f941bfcc360d68024167d7fe514
Artsem_Nikulin <07a.nikulin@gmail.com>
2024-10-07 18:07:29 +0200

    test

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-import json
-
-from deploy_module.deploy import DataScanManager
-from deploy_module.rules_reader import RulesReader
-import os
 from deploy_module.git_changes import get_changed_files
+from deploy_module.rules_reader import RulesReader
+from deploy_module.deploy import DataScanManager
+import os
 
+branch_name = os.getenv('BRANCH_NAME')
+base_branch = os.getenv('BASE_BRANCH')
+pr = os.getenv('PR')
 
-x = [
-    '/workspace/cloudbuild.yaml',
-    '/workspace/deploy_module/deploy.py',
-    '/workspace/main.py',
-    '/workspace/rules/datasets/DATASET_A/table_a/rules.yaml'
-]
+print("*********************************************")
+print(f"Getting GIT changes")
+print("*********************************************")
 
-# Filter the list for paths that end with 'rules.yaml'
+changed_files = get_changed_files()
+changed_rules = [changed_rules for changed_rules in changed_files if changed_rules.name == 'rules.yaml']
+print(changed_rules)
+
+if len(changed_rules) == 0:
+    print("There are no formalized DataScans, since no changes in Rules")
+    print(f"The following changes are detected: {changed_files}")
+
+else:
+    print("*********************************************")
+    print(f"Validation of PR #{pr}: {branch_name} TO {base_branch}")
+    print("*********************************************")
+
+    datasets_with_rules = RulesReader(changed_rules).get_datasets_with_rules()
+    DataScanManager(env='dev', datasets_with_rules=datasets_with_rules).create_data_scans(validate=True)
+
 
 
